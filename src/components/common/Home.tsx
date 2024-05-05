54e50f6c17e512a145e42a2dd606d9203ff731c5
matt-hulme <mattwhulme@gmail.com>
2024-05-05 15:26:31 -0600

    MUI installed

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -1,4 +1,5 @@
 export const Home = () => (
-  <div className="bg-blue-500">Home</div>
+  <div className="bg-cover bg-inset-0 bg-star-background flex h-screen items-center justify-center w-screen">
+    <p className="text-white">Home</p>
+  </div>
 )
-
