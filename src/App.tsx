dd3a3baa0532324fd6534b1c246880693f725fe4
matt-hulme <mattwhulme@gmail.com>
2024-05-08 16:39:48 -0600

    Homepage height fixed

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
     <ThemeProvider theme={theme}>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
       </Router>
     </ThemeProvider>
   )
