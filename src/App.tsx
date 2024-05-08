c10e0b24ce7f0ded567173c3c8f9a1be733022f5
matt-hulme <mattwhulme@gmail.com>
2024-05-07 20:48:32 -0600

    home scrollbar fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import { Home } from './components/Home'
 import { Navbar } from './components/common'
 import theme from './theme'
 import { ThemeProvider } from '@emotion/react'
-// import other pages here
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <div className='h-screen'>
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-    </div>
     </ThemeProvider>
   )
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,8 @@
 import { HomeCard } from "./HomeCard"
 
 export const Home = () => (
-  <div className="bg-fit bg-star-background flex flex-col h-full overflow-y-auto pt-16 w-screen">
-      <div className="grid grid-cols-6 grid-rows-6 h-[calc(100vh-16px)] min-h-[450px] p-10 lg:flex-row lg:min-h-[200px]">
+  <div className="bg-fit bg-star-background flex flex-col pt-16">
+      <div className="grid grid-cols-6 grid-rows-6 h-[calc(100vh-4rem)] min-h-[500px] overflow-y-auto p-10 md:min-h-[450px] lg:flex-row lg:min-h-[150px]">
         <div className="col-span-6 row-span-1 md:col-span-3 md:row-span-2 lg:col-span-1 lg:row-span-6">
           <HomeCard title='Characters'/>
         </div>
