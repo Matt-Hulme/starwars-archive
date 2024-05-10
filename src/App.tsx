7bc73859baf3ea9559958e4821f7af0e8a0e4edf
matt-hulme <mattwhulme@gmail.com>
2024-05-09 19:55:33 -0600

    Home Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import { Home } from './components/Home'
 import { Navbar } from './components/common'
 import theme from './theme'
 import { ThemeProvider } from '@emotion/react'
+import { CharactersList } from './components/Characters/CharactersList'
+import { FilmsList } from './components/Films/FilmsList'
+import { PlanetsList } from './components/Planets/PlanetsList'
+import { SpeciesList } from './components/Species/SpeciesList'
+import { StarshipsList } from './components/Starships/StarshipsList'
+import { VehiclesList } from './components/Vehicles/VechiclesList'
 
 function App() {
   return (
@@ -11,6 +17,12 @@ function App() {
         <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/characters" element={<CharactersList />} />
+            <Route path="/films" element={<FilmsList />} />
+            <Route path="/planets" element={<PlanetsList />} />
+            <Route path="/species" element={<SpeciesList />} />
+            <Route path="/starships" element={<StarshipsList />} />
+            <Route path="/vehicles" element={<VehiclesList />} />
           </Routes>
       </Router>
     </ThemeProvider>
---
+++ src/components/Films/FilmsList.tsx
@@ -0,0 +1,6 @@
+export const FilmsList = () =>   
+  <div className="bg-fit bg-star-background flex flex-col pt-16">
+    <div className="h-[calc(100vh-64px)] bg-yellow-100 w-full p-5">
+      FilmsList
+    </div>
+  </div>
