73ddc47f30e9457c44f79446323797b415ca73dd
matt-hulme <mattwhulme@gmail.com>
2024-05-13 20:15:46 -0600

    CharactersList done

--- src/components/Characters/CharactersList.tsx
+++ src/components/Characters/CharactersList.tsx
@@ -1,6 +1,11 @@
 import { ArrowBack } from '@mui/icons-material'
-import { Button } from '@mui/material'
+import { Button, ImageList, ImageListItem } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+
+const imagePaths = Array.from(
+  { length: 83 },
+  (_, i) => `src/assets/images/characters/${i + 1}.jpg`,
+)
 
 export const CharactersList = () => {
   const navigate = useNavigate()
@@ -10,18 +15,33 @@ export const CharactersList = () => {
   }
 
   return (
-    <div className="bg-fit bg-star-background flex flex-col pt-16">
-      <div className="h-[calc(100vh-64px)] bg-black-100 w-full px-10 pt-5">
-        <Button
-          className="duration-800 ease-linear opacity-80 transition-all hover:opacity-100"
-          onClick={onClick}
-          size="large"
-          startIcon={<ArrowBack />}
-          sx={{ color: '#FFBE00B3' }}
-          variant="text"
-        >
-          Back
-        </Button>
+    <div className="bg-fit bg-star-background pt-16">
+      <div className="h-[calc(100vh-64px)] flex flex-row bg-black-100 pr-[84px] w-full p-5">
+        <div>
+          <Button
+            className="duration-800 ease-linear opacity-80 transition-all hover:opacity-100"
+            onClick={onClick}
+            size="large"
+            startIcon={<ArrowBack />}
+            sx={{ color: '#FFBE00B3' }}
+            variant="text"
+          >
+            Back
+          </Button>
+        </div>
+        <div className="grow bg-blue-400 rounded-lg">
+          <ImageList
+            sx={{ width: '100%', height: '100%' }}
+            cols={6}
+            rowHeight={300}
+          >
+            {imagePaths.map((item, index) => (
+              <ImageListItem key={index}>
+                <img src={item} alt={`character${index + 1}`} />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </div>
       </div>
     </div>
   )
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,7 @@ import { HomeCard } from './HomeCard'
 
 export const Home = () => (
   <div className="bg-fit bg-star-background flex flex-col pt-16">
-    <div className="grid grid-cols-6 grid-rows-6 h-[calc(100vh-64px)] min-h-[500px] overflow-y-auto px-10 pt-5 md:min-h-[450px] lg:flex-row lg:min-h-[150px]">
+    <div className="grid grid-cols-6 grid-rows-6 h-[calc(100vh-64px)] min-h-[500px] overflow-y-auto px-[58px] pt-5 md:min-h-[450px] lg:flex-row lg:min-h-[150px]">
       <div className="col-span-6 row-span-1 md:col-span-3 md:row-span-2 lg:col-span-1 lg:row-span-6">
         <HomeCard title="Characters" />
       </div>
@@ -16,10 +16,10 @@ export const Home = () => (
         <HomeCard title="Species" />
       </div>
       <div className="col-span-6 row-span-1 md:col-span-3 md:row-span-2 lg:col-span-1 lg:row-span-6">
-        <HomeCard title="Vehicles" />
+        <HomeCard title="Starships" />
       </div>
       <div className="col-span-6 row-span-1 md:col-span-3 md:row-span-2 lg:col-span-1 lg:row-span-6">
-        <HomeCard title="Starships" />
+        <HomeCard title="Vehicles" />
       </div>
     </div>
   </div>
