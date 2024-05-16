3298dcb00bea71796eee02c67e3d86a620c4e2ee
matt-hulme <mattwhulme@gmail.com>
2024-05-15 23:01:43 -0600

    Film Posters

--- src/components/Films/FilmsList.tsx
+++ src/components/Films/FilmsList.tsx
@@ -1,5 +1,4 @@
 import { ImageList, ImageListItem } from '@mui/material'
-// import { useNavigate } from 'react-router-dom'
 
 const filmsImgs = Array.from(
   { length: 6 },
@@ -7,52 +6,48 @@ const filmsImgs = Array.from(
 )
 
 export const FilmsList = () => {
-  // const navigate = useNavigate()
-
-  // const onClick = () => {
-  //   navigate('/')
-  // }
-
   return (
-    <div className="bg-fit bg-star-background flex flex-col h-full pt-16">
-      <div className="block px-10 rounded-lg md:hidden">
-        <ImageList sx={{ width: '100%' }} cols={1}>
-          {filmsImgs.map((item, index) => (
-            <ImageListItem key={index}>
-              <img
-                src={item}
-                alt={`films${index + 1}`}
-                className="rounded-lg"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </div>
-      <div className="hidden px-10 rounded-lg md:block lg:hidden">
-        <ImageList sx={{ width: '100%' }} cols={3}>
-          {filmsImgs.map((item, index) => (
-            <ImageListItem key={index}>
-              <img
-                src={item}
-                alt={`films${index + 1}`}
-                className="rounded-lg"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </div>
-      <div className="hidden px-10 rounded-lg lg:block">
-        <ImageList sx={{ width: '100%' }} cols={5}>
-          {filmsImgs.map((item, index) => (
-            <ImageListItem key={index}>
-              <img
-                src={item}
-                alt={`films${index + 1}`}
-                className="rounded-lg"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
+    <div className="bg-fit h-screen bg-star-background flex flex-col pt-16">
+      <div className="h-screen overflow-y-auto">
+        <div className="block px-10 rounded-lg md:hidden">
+          <ImageList cols={1}>
+            {filmsImgs.map((item, index) => (
+              <ImageListItem key={index}>
+                <img
+                  src={item}
+                  alt={`films${index + 1}`}
+                  className="rounded-lg"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </div>
+        <div className="hidden px-10 rounded-lg md:block lg:hidden">
+          <ImageList cols={2}>
+            {filmsImgs.map((item, index) => (
+              <ImageListItem key={index}>
+                <img
+                  src={item}
+                  alt={`films${index + 1}`}
+                  className="rounded-lg"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </div>
+        <div className="hidden px-10 rounded-lg md:block">
+          <ImageList cols={3}>
+            {filmsImgs.map((item, index) => (
+              <ImageListItem key={index}>
+                <img
+                  src={item}
+                  alt={`films${index + 1}`}
+                  className="rounded-lg"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </div>
       </div>
     </div>
   )
