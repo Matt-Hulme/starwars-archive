81e2717c1ee50ec49c5d91ee17f0caf6ea812bd2
matt-hulme <mattwhulme@gmail.com>
2024-05-19 12:52:13 -0600

    panel layout

--- src/components/Characters/CharacterDetails.tsx
+++ src/components/Characters/CharacterDetails.tsx
@@ -1,3 +1,12 @@
 export const CharacterDetails = () => {
-  return <div>CharacterDetails</div>
+  return (
+    <div className="bg-fit bg-fixed bg-star-background flex flex-col min-h-screen px-10 pt-[104px]">
+      <div className="w-full space-y-5">
+        <header className="bg-gray-100">Character Header</header>
+        <section className="bg-gray-100">Character Films</section>
+        <section className="bg-gray-100">Character Vehicles</section>
+        <section className="bg-gray-100">Character Starships</section>
+      </div>
+    </div>
+  )
 }
--- src/components/Vehicles/VehicleDetails.tsx
+++ src/components/Vehicles/VehicleDetails.tsx
@@ -1,3 +1,11 @@
 export const VehicleDetails = () => {
-  return <div>VehicleDetails</div>
+  return (
+    <div className="bg-fit bg-fixed bg-star-background flex flex-col min-h-screen px-10 pt-[104px]">
+      <div className="w-full space-y-5">
+        <header className="bg-gray-100">Vehicle Header</header>
+        <section className="bg-gray-100">Vehicle Pilots</section>
+        <section className="bg-gray-100">Vehicle Films</section>
+      </div>
+    </div>
+  )
 }
