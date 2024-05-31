64fd9a8cef45fc4efcd9cea04e2f540222de0548
matt-hulme <mattwhulme@gmail.com>
2024-05-30 20:37:39 -0600

    DetailsHeader init

--- src/components/common/DetailsHeader/DetailsHeader.tsx
+++ src/components/common/DetailsHeader/DetailsHeader.tsx
@@ -1,3 +1,12 @@
-export const DetailsHeader = () => {
-  return <header></header>
+interface DetailsHeaderProps {
+  image: string
+  name: string
+}
+
+export const DetailsHeader = ({ image, name }: DetailsHeaderProps) => {
+  return (
+    <header className="w-[400px] h-400px object-contain">
+      <img className="rounded-full" alt={name ?? ''} src={image} />
+    </header>
+  )
 }
